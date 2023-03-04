111e98c2c29f056a638220680eaac7d98a720552
CatherineJOnia <catherinejonia@gmail.com>
2023-03-04 16:18:39 -0500

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 import '../styles/globals.css'
 import SideBar from "../components/SideBar"
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
 
-export default function Rootlayout({
+export default async function Rootlayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+        <div className="fle)}x">
           <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
             <SideBar />
           </div>
@@ -18,6 +28,8 @@ export default function Rootlayout({
           {/* ClientProvider - Notification */}
           <div className="bg-[#343541] flex-1">{children}</div>
           </div>
+          )}
+          </SessionProvider>
       </body>
     </html>
   )

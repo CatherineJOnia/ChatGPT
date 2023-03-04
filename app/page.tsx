90c563f20d10d47ec2bcee826e4dff2bd2f20554
CatherineJOnia <catherinejonia@gmail.com>
2023-03-04 13:26:57 -0500

    landing page and sidebar with icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,53 @@
+import { SunIcon, BoltIcon, ExclamationTriangleIcon } from "@heroIcons/react/24/outline";
+
+
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
-        <h1 className="text-5xl font-bold mb-20">
-          Cat's ChatGPT
+        <h1 className="text-5xl font-bold mb-10">
+          ChatGPT
         </h1>
-        <div>
+        <h3 className="text-xl mb-10 justify-center">
+          Developed by Catherine Onia
+        </h3>
+        <div className="flex space-x-2 text-center">
           <div>
-            <div>
+            <div className="flex flex-col items-center justify-center mb-5">
+              <SunIcon className="h-8 w-8" />
               <h2>Examples</h2>
             </div>
 
+            <div className="space-y-2">
+            <p className="infoText">"Explain the difference between the MERN stack and the PERN stack"</p>
+              <p className="infoText">"What are five behavioral interview questions I should practice?"</p>
+              <p className="infoText">"Write two bullets for my resume based on [these] job qualifications"</p>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-col items-center justify-center mb-5">
+              <BoltIcon className="h-8 w-8" />
+              <h2>Capabilities</h2>
+            </div>
+
+            <div className="space-y-2">
+            <p className="infoText">Experiment with different ChatGPT Models</p>
+              <p className="infoText">Messages are stored in Firebase's Firestore</p>
+              <p className="infoText">Hot Toast notifications when ChatGPT is thinking</p>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-col items-center justify-center mb-5">
+              <ExclamationTriangleIcon className="h-8 w-8" />
+              <h2>Limitations</h2>
+            </div>
+
+            <div className="space-y-2">
+              <p className="infoText">May occasionally generate incorrect information</p>
+              <p className="infoText">May occasionally product harmful instructions of biased content</p>
+              <p className="infoText">Limited knowledge of world and events after 2021</p>
+            </div>
           </div>
         </div>
     </div>
---
+++ components/SideBar.tsx
@@ -0,0 +1,24 @@
+import NewChat from "./NewChat"
+
+function SideBar() {
+  return (
+  <div className="p-2 flex flec-col h-screen">
+    <div className="flex-1">
+      <div>
+
+        {/* New Chat */}
+        <NewChat />
+        <div>
+          {/* Model Selection */}
+        </div>
+
+        {/* Map through the ChatRows */}
+
+
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default SideBar;

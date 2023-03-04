28aed0d5660b4bdd1298951222bce6f6c18ebb32
CatherineJOnia <catherinejonia@gmail.com>
2023-03-04 18:59:22 -0500

    new chatand delete chat

--- app/chat / [id]/page.tsx
+++ app/chat / [id]/page.tsx
@@ -1,6 +1,6 @@
 function ChatPage() {
   return (
-  <div>CharPage</div>
+  <div>ChatPage</div>
   )
 };
 
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,9 +3,21 @@
 import NewChat from "./NewChat"
 import { useSession, signOut } from "next-auth/react";
 import DevelopedBy from "./DevelopedBy";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
 
   return (
   <div id="side-bar" className="p-2 flex flex-col h-screen">
@@ -17,7 +29,9 @@ function SideBar() {
           {/* Model Selection */}
         </div>
 
-        {/* Map through the ChatRows */}
+        {chats?.docs.map((chat) => {
+          return <ChatRow key={chat.id} id={chat.id} />
+        })}
 
       </div>
       </div>
@@ -33,7 +47,9 @@ function SideBar() {
             alt="profile pic"
             className="h-13 w-13 md:h-15 md:w-15 rounded-full mx-auto"
           />
-          <div className="w-15 h-15 py-2 px-6 text-sm rounded-lg text-white my-5 border border-gray-700  hover:bg-gray-700/70 transition-all duration-200 ease-outrounded-lg ">
+          <div
+            className="w-15 h-15 py-2 px-6 text-sm rounded-lg text-white my-5 border border-gray-700  hover:bg-gray-700/70 transition-all duration-200 ease-outrounded-lg "
+          >
             <button onClick={() => signOut()}>Sign Out</button>
           </div>
           <div>

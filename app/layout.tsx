221db45fbe71cb24d5ff3d67258764148e856780
CatherineJOnia <catherinejonia@gmail.com>
2023-03-04 09:09:38 -0500

    boilerplate

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+
+export default function Rootlayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <div className="flex">
+          {/* Sidebar */}
+          {/* ClientProvider - Notification */}
+          <div className="bg-[#343541] flex-1">{children}</div>
+          </div>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+        <h1 className="text-5xl font-bold mb-20">
+          Cat's ChatGPT
+        </h1>
+        <div>
+          <div>
+            <div>
+              <h2>Examples</h2>
+            </div>
+
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage

034fbf65d36cef230dfd2940aeb83bf71056f134
CatherineJOnia <catherinejonia@gmail.com>
2023-03-04 19:30:53 -0500

    toast installed, chat input

---
+++ components/ChatInput.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { FormEvent, useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import { db } from "../firebase";
+import ModelSelection from "./ModelSelection";
+import useSWR from "swr";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  chatId?: string;
+};
+
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  // useSWR to get model
+  const model = "text-davinci-003"
+
+  const sendMessage = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId!,
+        "messages"
+      ),
+      message
+    );
+
+    //Toast loading
+
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    })
+      .then(() => {
+        // Toast successful
+      })
+  };
+
+  return (
+    <div className="bg-gray-700/90 text-gray-400 rounded-lg text-sm  sticky bottom-2 mt-2">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
+        <input
+            className=" bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed  disabled:text-gray-300  text-white"
+            disabled={!session}
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            type="text"
+            placeholder="Type your message here...."
+          />
+
+          <button
+            disabled={!prompt || !session}
+            className="bg-[#11a37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:text-gray-300 disabled:cursor-not-allowed"
+            type="submit"
+          >
+            <PaperAirplaneIcon className="h-4 2-4 -rotate-45" />
+          </button>
+      </form>
+    </div>
+
+  )
+}
+
+export default ChatInput;
